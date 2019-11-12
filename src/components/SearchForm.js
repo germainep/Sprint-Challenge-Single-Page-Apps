70f4f5602842d333bd8a761ca90fd4389eda5c5f
Germaine Pannell <me@germainep.com>
2019-11-12 14:42:23 -0500

    set up input handler for search field

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,10 +3,15 @@ import React, { useState } from 'react';
 import { InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
 
 export default function SearchForm() {
+  const [search, setSearch] = useState('');
+
+  const handleChange = event => {
+    setSearch({ ...search, [event.target.name]: event.target.value });
+  };
   return (
     <section className="search-form">
       <InputGroup>
-        <Input type="text" name="search" onChange={event => handleChange(event)} />
+        <Input type="text" name="search-field" onChange={event => handleChange(event)} />
         <InputGroupAddon addonType="prepend">
           <Button>Search</Button>
         </InputGroupAddon>
